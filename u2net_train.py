8a879d7d0dce130c7af56b556a113ec167dd2986
Sanghyun <sanghyun@xbrain.team>
2023-02-08 12:04:15 +0900

    update train batch size

--- u2net_train.py
+++ u2net_train.py
@@ -59,7 +59,7 @@ label_ext = '_mask.gif'
 model_dir = os.path.join(os.getcwd(), 'saved_models', model_name + os.sep)
 
 epoch_num = 100000
-batch_size_train = 12
+batch_size_train = 32
 batch_size_val = 1
 train_num = 0
 val_num = 0
